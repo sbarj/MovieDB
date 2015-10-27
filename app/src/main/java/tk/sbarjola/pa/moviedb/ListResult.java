fe095f271435ee1e87a506b91cabdac4f04cdfda
Sergi <sergibca@gmail.com>
2015-10-27 19:49:51 +0100

    Afegit dos Arxius faltants "ListResult i Result"

---
+++ app/src/main/java/tk/sbarjola/pa/moviedb/ListResult.java
@@ -0,0 +1,82 @@
+package tk.sbarjola.pa.moviedb;
+
+/**
+ * Created by 46465442z on 27/10/15.
+ */
+        import java.util.ArrayList;
+        import java.util.HashMap;
+        import java.util.List;
+        import java.util.Map;
+
+public class ListResult {
+
+    private Integer page;
+    private List<Result> results = new ArrayList<Result>();
+    private Integer totalPages;
+    private Integer totalResults;
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    /**
+     * @return The page
+     */
+    public Integer getPage() {
+        return page;
+    }
+
+    /**
+     * @param page The page
+     */
+    public void setPage(Integer page) {
+        this.page = page;
+    }
+
+    /**
+     * @return The results
+     */
+    public List<Result> getResults() {
+        return results;
+    }
+
+    /**
+     * @param results The results
+     */
+    public void setResults(List<Result> results) {
+        this.results = results;
+    }
+
+    /**
+     * @return The totalPages
+     */
+    public Integer getTotalPages() {
+        return totalPages;
+    }
+
+    /**
+     * @param totalPages The total_pages
+     */
+    public void setTotalPages(Integer totalPages) {
+        this.totalPages = totalPages;
+    }
+
+    /**
+     * @return The totalResults
+     */
+    public Integer getTotalResults() {
+        return totalResults;
+    }
+
+    /**
+     * @param totalResults The total_results
+     */
+    public void setTotalResults(Integer totalResults) {
+        this.totalResults = totalResults;
+    }
+
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+}
